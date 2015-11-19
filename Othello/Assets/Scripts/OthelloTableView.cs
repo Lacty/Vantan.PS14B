f4d9c3d81b6c6eac7dcd81568aaaa17766f092f2
Leon Akasaka <leon@wisdomsoft.jp>
2015-11-19 19:16:03 +0900

    石を置けるかどうかの判定を追加。

--- Othello/Assets/Scripts/OthelloTableView.cs
+++ Othello/Assets/Scripts/OthelloTableView.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class OthelloTableView : MonoBehaviour
 {
@@ -51,6 +52,18 @@ public class OthelloTableView : MonoBehaviour
         }
     }
 
+    [SerializeField]
+    private Material _placeableMaterial = null;
+    public Material PlaceableMaterial
+    {
+        get
+        {
+            if (_placeableMaterial != null) { return _placeableMaterial; }
+            _placeableMaterial = Resources.Load<Material>("Cell/Placeable");
+            return _placeableMaterial;
+        }
+    }
+
     private const int Rows = 8;
     private const int Columns = 8;
     private readonly GameObject[,] _cells = new GameObject[Rows, Columns];
@@ -70,8 +83,21 @@ public class OthelloTableView : MonoBehaviour
         UpdateCellState(4, 3, CellState.White);
         UpdateCellState(4, 4, CellState.Black);
 
-
         UpdateSelectedCell(0, 0);
+        UpdatePlaceableCells();
+    }
+
+    private void UpdatePlaceableCells()
+    {
+        for (var r = 0; r < Rows; r++)
+        {
+            for (var c = 0; c < Columns; c++)
+            {
+                var renderer = _cells[r, c].GetComponent<Renderer>();
+                renderer.material = IsPlaceable(r, c, _currentPlayer) ?
+                    PlaceableMaterial : NormalMaterial;
+            }
+        }
     }
 
     private GameObject CreateGameObject(GameObject origin, int r, int c)
@@ -143,15 +169,65 @@ public class OthelloTableView : MonoBehaviour
             { UpdateSelectedCell(SelectedRow, SelectedColumn + 1); }
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            var stone = _stones[SelectedRow, SelectedColumn];
-            if (stone == null)
+            if (IsPlaceable(SelectedRow, SelectedColumn, _currentPlayer))
             {
                 UpdateCellState(
                     SelectedRow, SelectedColumn,
                     ToCellState(_currentPlayer));
                 _currentPlayer = GetOtherPlayer(_currentPlayer);
+                UpdatePlaceableCells();
             }
         }
+    }
+
+    private bool IsPlaceable(int row, int column, Player player)
+    {
+        var state = GetCellState(row, column);
+        if (state != CellState.None) { return false; }
+        
+        return
+            IsPlaceableByDirection(row, column, -1, -1, player) || //左上
+            IsPlaceableByDirection(row, column, -1, 0, player) || //上
+            IsPlaceableByDirection(row, column, -1, 1, player) || //右上
+            IsPlaceableByDirection(row, column, 0, -1, player) || //左
+            IsPlaceableByDirection(row, column, 0, 1, player) || //右
+            IsPlaceableByDirection(row, column, 1, -1, player) || //左下
+            IsPlaceableByDirection(row, column, 1, 0, player) || //下
+            IsPlaceableByDirection(row, column, 1, 1, player); //右;
+    }
+
+    private bool IsPlaceableByDirection(int row, int column, int dr, int dc, Player player)
+    {
+        var other = ToCellState(GetOtherPlayer(player));
+
+        var nr = row + dr;
+        var nc = column + dc;
+        if (GetCellState(nr, nc) != other) { return false; }
+
+        for (
+            int ir = nr + dr, ic = nc + dc;
+            ir >= 0 && ic >= 0 && ir < Rows && ic < Columns;
+            ir += dr, ic += dc)
+        {
+            var s = GetCellState(ir, ic);
+            if (s != other) { return s != CellState.None; }
+        }
+
+        return false;
+    }
+
+    private CellState GetCellState(int row, int column)
+    {
+        if (row < 0 || row >= Rows || column < 0 || column >= Columns)
+        {
+            return CellState.None;
+        }
+
+        var stone = _stones[row, column];
+        return stone == null ? CellState.None
+            : stone.transform.localRotation == Quaternion.identity
+                ? CellState.Black
+            : CellState.White;
     }
 
     private void UpdateCellState(int row, int column, CellState cellState)
@@ -175,9 +251,13 @@ public class OthelloTableView : MonoBehaviour
 
     private void UpdateSelectedCell(int row, int column)
     {
-        SelectedCell.GetComponent<Renderer>().material = NormalMaterial;
+        SelectedCell.GetComponent<Renderer>().material
+            = IsPlaceable(SelectedRow, SelectedColumn, _currentPlayer) ?
+                PlaceableMaterial : NormalMaterial;
         SelectedRow = row;
         SelectedColumn = column;
         SelectedCell.GetComponent<Renderer>().material = SelectedMaterial;
     }
+
+
 }
